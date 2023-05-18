83e68b7b0f36ff68e6af8838d051ebe96523766e
Maciej Szymczyk <zorteran@gmail.com>
2023-05-18 21:12:20 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-docker compose up -d elasticsearch
-sleep 60 # TODO: make it more elegant
+
+# Check if Docker command can be executed
+if ! docker compose up -d elasticsearch; then
+    echo "Looks like you don't have permission to use Docker."
+    exit 1
+fi
+
+# Wait for Elasticsearch to be available with a timeout
+timeout=60
+elapsed=0
+
+until curl --insecure --silent --output /dev/null https://localhost:9200; do
+    echo "Waiting for Elasticsearch to start..."
+    sleep 5
+    ((elapsed+=5))
+
+    if ((elapsed >= timeout)); then
+        echo "Timed out waiting for Elasticsearch to start."
+        exit 1
+    fi
+done
+
 docker exec elasticsearch /bin/bash -c "bin/elasticsearch-setup-passwords auto --batch --url https://localhost:9200" >> creds.txt
-NEW_PASS=$(cat creds.txt | grep "PASSWORD kibana_system" | awk '{print substr($0,length($0)-19,length($0))}')
+NEW_PASS=$(grep "PASSWORD kibana_system" creds.txt | awk '{print substr($0, length($0)-19, length($0))}')
 sed -i "s/CHANGE_ME_PLS/$NEW_PASS/" docker-compose.yml
 docker compose up -d kibana
